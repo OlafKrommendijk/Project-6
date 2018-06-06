fa6835432b7c34175ecf12a81ea912073400489b
unknown <oh.krommendijk@student.alfa-college.nl>
2018-06-06 11:36:39 +0200

    contact formulier goed gemaakt

--- Website/contact.js
+++ Website/contact.js
@@ -3,17 +3,19 @@ function controle(){
 	var email = mail.toString();
 	var email1= email.toLowerCase();
 
+	var tekst = document.getElementById('Bericht').value;
+
 	var apenstaartje = email1.indexOf("@");
 		if (apenstaartje !== -1){
 				var domein = email1.indexOf(".nl");
 				var domeinB = email1.indexOf(".com");
 
 				if (domein !== -1){
-					window.location.replace("http://www.ompatelefoons.icthardenberg.nl/Contactverstuurd.html")
+					document.getElementById("formulier").innerHTML = "<h2> Uw Bericht is verzonden </h2> <br />" + tekst;
 				}
 
 				else if (domeinB !== -1) {
-					window.location.replace("http://www.ompatelefoons.icthardenberg.nl/Contactverstuurd.html")
+					document.getElementById("Formulier").innerHTML = "<h2> Uw Bericht is verzonden </h2> <br />" + tekst;
 				}
 
 				else alert("voer een geldig domein in");
