352726c225f7e0b8f6464b19ac3cddbec3daf24b
LeviZandbergen <33252026+LeviZandbergen@users.noreply.github.com>
2018-06-06 09:48:56 +0200

    personeel pagina in php gemaakt

---
+++ Website/Personeel.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+	<HTML>
+		<HEAD>
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<LINK href="Sjabloon.css" rel="stylesheet" type="text/css" />
+			<meta name="viewport" content="width=device-width, initial-scale=1">
+			<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+			<TITLE> Ompa Telefoons </TITLE>
+		</HEAD>
+			<BODY>
+			<div id="container">
+				<div id="header">
+				<a href="index.html"> <img src="logo.png" id="logo" alt="Logo" title="Logo" /></a>	
+					<div id="HText">
+						<h2><strong>INLOGGEN</strong></h2>
+					</div>
+						<div id="zoeken">
+								<input type="text" id="InloggenBox" placeholder="zoeken">
+									<button>Zoeken</button><button id="knop" <i class="fa fa-shopping-cart"></i></button>
+						</div>
+
+				</div>
+					<div class="nav">
+						<div class="Home">
+							
+
+							<a class="dropbtn" href="index.html"><strong>HOME</strong></a></button>
+							<a class="dropbtn" href="Informatie.html"><strong>INFORMATIE</strong></a></button>
+							<a class="dropbtn" href="Producten.html"><strong>PRODUCTEN</strong></a></button>
+							<a class="dropbtn" href="Contact.html"><strong>CONTACT</strong></a></button>
+							<a class="dropbtn" href="Inloggen.html"><strong>INLOGGEN</strong></a></button>
+							<a class="dropbtn" href="Personeel.html"><strong>PERSONEEL</strong></a></button>
+						</div>
+					</div>
+					<footer>
+						<div id="footer">
+							<div id="FooterText">
+								<p>&copy; Copyright 2018 Ompa Telefoons</p>
+							</div>
+						</div>
+					</footer>
+					<?php
+	$Voornaam = array("Jan", "Piet", "Jaap", "Henk", "Joep", "Gea", "Tea", "Fien", "Olaf", "Tim", "Jesse", "Rob", "Robin", "Max", "Niels");
+
+       echo "<td>" . "<strong>Ons Personeel:</strong>" . "</td>";
+	foreach ($Voornaam as $value) {
+		echo "<br>";
+		echo "<img id='one' src='nobody.JPG'>";
+		echo "<br>";
+        echo "<td>" . "<strong>Voornaam:</strong> <i>$value</i>" . "</td>";
+        echo "<br>";
+        echo "<td>" . "<strong>Achternaam:</strong> <i>Familie</i>" . "</td>";
+        echo "<br>";
+        echo "<td>" . "<strong>Werkende als:</strong> <i>JEEEEEE</i>" . "</td>";
+		}
+	?>
+			</BODY>
+	</html>
